7ea92337c87ce4b768b0fb1620c15a76ff1b5159
Emmanuel Perea <archivobelal@gmail.com>
2022-01-21 15:04:33 -0500

    Terminando formulario

--- vistas/paginas/error404.php
+++ vistas/paginas/error404.php
@@ -1,5 +1,5 @@
 <div class="text-center">
-<h1>404</h1>
-<p>Página no encontrada</p>
-<a href="index.php?pagina=formulario" class="btn btn-primary">Volver</a>
+	<h1>404</h1>
+	<p>Página no encontrada</p>
+	<a href="index.php?pagina=formulario" class="btn btn-primary">Volver</a>
 </div>
--- vistas/paginas/formulario.php
+++ vistas/paginas/formulario.php
@@ -1,18 +1,99 @@
-<h1>Formulario Ingreso</h1>
+<div class="d-flex justify-content-center" >
 
-<form >
+<form class="py-5 bg-light" method="post" style="width:80%;">
+
+	<!-- ENTRADA PARA EL NOMBRE-->
+
+
 	<div class="mb-3 mt-3">
-		<label for="email" class="form-label">Email:</label>
-		<input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+		<label for="nombre" class="form-label">Nombre completo *</label>
+		<input type="text" class="form-control" id="nombre" placeholder="Nombre completo del empleado" name="nombre" required>
 	</div>
-	<div class="mb-3">
-		<label for="pwd" class="form-label">Password:</label>
-		<input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
+
+	<!-- ENTRADA PARA EL CORREO-->
+
+	<div class="mb-3 mt-3">
+		<label for="email" class="form-label">Correo electrónico *</label>
+		<input type="email" class="form-control" id="email" placeholder="Correo electrónico" name="email" required>
 	</div>
+
+	<!-- ENTRADA PARA SEXO-->
+
+	<label>Sexo *</label>
+
+	<div class="form-check">
+
+		<input type="radio" class="form-check-input" id="radio1" name="optradio" value="option1" required>Masculino
+		<label class="form-check-label" for="radio1"></label>
+	</div>
+
+	<div class="form-check">
+		<input type="radio" class="form-check-input" id="radio2" name="optradio" value="option2">Femenino
+		<label class="form-check-label" for="radio2"></label>
+	</div>	
+
+	<!-- ENTRADA PARA EL AREA-->
+
+	<label>Area *</label>
+
+                              <select class="form-control" name="fechadestruccion"  >
+
+                              <option value="">-- SELECCIONE --</option>
+
+                              <option value="PAPEL">PAPEL</option>
+                              <option value="ELECTRONICO">ELECTRONICO</option>
+                              <option value="HIBRIDO">HIBRIDO</option>
+
+
+                            </select>                    
+
+	<!-- ENTRADA PARA EL OBSERVACIONES -->
+
+	<div class="form-group">
+
+		<label>Descripción *</label>
+
+		<div class="input-group">
+
+			
+			<textarea class="form-control" name="nuevaObservacion" rows="2" placeholder="Descripción de la experiencia del empleado" required></textarea>
+
+
+		</div>
+
+	</div>
+
+	<!-- ENTRADA PARA EL BOLETIN INFORMATIVO -->
+
+
 	<div class="form-check mb-3">
+
 		<label class="form-check-label">
-			<input class="form-check-input" type="checkbox" name="remember"> Remember me
+			<input class="form-check-input" type="checkbox" name="remember" required=""> Deseo recibir boletin informativo
 		</label>
 	</div>
-	<button type="submit" class="btn btn-primary">Submit</button>
+
+	<!-- ENTRADA PARA ROLES -->
+
+	<label>Roles *</label>
+
+	<div class="form-check">
+		<input type="checkbox" class="form-check-input" id="check1" name="option1" value="something">
+		<label class="form-check-label" for="check1">Option 1</label>
+	</div>
+	<div class="form-check">
+		<input type="checkbox" class="form-check-input" id="check2" name="option2" value="something">
+		<label class="form-check-label" for="check2">Option 2</label>
+	</div>
+	<div class="form-check">
+		<input type="checkbox" class="form-check-input">
+		<label class="form-check-label">Option 3</label>
+	</div>
+
+	<br>
+
+	<button type="submit" class="btn btn-primary">Enviar</button>
+
 </form>
+
+</div>
